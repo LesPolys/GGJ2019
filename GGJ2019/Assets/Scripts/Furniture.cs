5389b8e9c0d754cbab38358ae6eafe8003958fca
lespolys <stewart.jlt@gmail.com>
2019-01-26 15:58:59 -0500

    no message

---
+++ GGJ2019/Assets/Scripts/Furniture.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Furniture : MonoBehaviour
+{
+
+    Vector3 finalPosition;
+    Quaternion finalRotation;
+
+    Vector3 lastPosition;
+
+    public float maxScoreVale;
+    public float scoreCheckCountDown;
+    public const float minDistValue = 10.0f;
+    private bool isMoving;
+    private bool isCountingDown;
+
+    private void Update()
+    {
+        if (transform.position != lastPosition) // has it started moving
+        {
+            transform.position = lastPosition;
+            isMoving = true;
+
+        }
+
+        if (isMoving && transform.position == lastPosition) { // has it stopped moving. 
+            isMoving = false;
+            isCountingDown = true;
+        }
+
+        if (isCountingDown)
+        {
+            scoreCheckCountDown -= Time.deltaTime;
+            if(scoreCheckCountDown <= 0)
+            {
+                RunScoreCheck();
+            }
+        }
+
+    }
+
+    public void SetFinalValues()
+    {
+        finalPosition = transform.position;
+        finalRotation = transform.rotation;
+    }
+
+
+    private void RunScoreCheck()
+    {
+        float finalDistance = Vector3.Distance(transform.position, finalPosition);
+
+
+   
+    }
+
+
+
+}
---
+++ GGJ2019/Assets/Scripts/FurnitureEditor.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[CustomEditor(typeof(Furniture))]
+public class FurnitureEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+
+        Furniture myScript = (Furniture)target;
+        if (GUILayout.Button("Set Final Values"))
+        {
+            myScript.SetFinalValues();
+        }
+        DrawDefaultInspector();
+
+    }
+}
+
