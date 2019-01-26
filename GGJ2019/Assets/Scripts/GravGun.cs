e9ef5fe306e624f7a12ca2f70591dd7d3caf6cbc
lespolys <stewart.jlt@gmail.com>
2019-01-26 12:16:28 -0500

    ?

--- GGJ2019/Assets/Scripts/GravGun.cs
+++ GGJ2019/Assets/Scripts/GravGun.cs
@@ -36,7 +36,7 @@ public class GravGun : MonoBehaviour
     private Vector2 rotationInput;
 
     /// <summary>The maximum distance at which a new object can be picked up</summary>
-    private const float maxGrabDistance = 30;
+    public float maxGrabDistance = 30;
 
     /// <returns>Ray from center of the main camera's viewport forward</returns>
     private Ray CenterRay()
