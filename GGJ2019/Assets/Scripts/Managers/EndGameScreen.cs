2a1bf19f5b542118f0b8f4bde4d6cf4df5631b02
Sean Murphy <seanmurp19@gmail.com>
2019-01-26 20:45:54 -0500

    Changes containing the the End Game Screen

---
+++ GGJ2019/Assets/Scripts/Managers/EndGameScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameScreen : MonoBehaviour
+{
+    [SerializeField]
+    private Transform RankText = null;
+
+    [SerializeField]
+    private Transform RestartButton = null;
+
+    [SerializeField]
+    private Transform QuitButton = null;
+
+    [SerializeField]
+    private Transform ReceiptRoot = null;
+
+    [SerializeField]
+    private Text ReceiptText = null;
+
+    // Starts the opening animations
+    private void OnEnable() { }
+
+
+    
+    #region Coroutines
+
+    private IEnumerator Coroutine_PopIn() { yield return null; }
+
+    private IEnumerator Coroutine_PopOut() { yield return null; }
+
+    #endregion
+}
