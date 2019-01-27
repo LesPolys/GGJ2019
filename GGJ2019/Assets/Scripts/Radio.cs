d38a7c3545e3715db1230cff7ab949d3e0994a1c
Jordan Kloosterman <Jordanklooster+Git@gmail.com>
2019-01-26 21:50:37 -0500

    Added some music and radio stuff

---
+++ GGJ2019/Assets/Scripts/Radio.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radio : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    AudioSource radioAudioSource;
+
+
+    private float songStartTime;
+    float songLength;
+
+    float delayBetweenSongs = 0.4f;
+
+    int currentClip = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        radioAudioSource = GetComponent<AudioSource>();
+
+        playSong(currentClip);
+        
+
+            
+
+    }
+
+
+    private void playSong(int clipToPlay)
+    {
+
+        radioAudioSource.clip = audioClips[clipToPlay];
+        songStartTime = Time.time;
+        songLength = radioAudioSource.clip.length;
+        radioAudioSource.Play();
+
+
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        CheckForSongEnd();
+    }
+
+
+    void CheckForSongEnd ()
+    {
+
+        if (Time.time >= songStartTime + songLength +  delayBetweenSongs)
+        {
+
+            currentClip += 1;
+            playSong(currentClip);
+            //change clip
+            //play song
+        }
+
+    }
+
+
+}
